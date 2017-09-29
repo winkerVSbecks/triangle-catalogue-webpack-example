475a2028c0c82fbf6a8ae7e52ed79b19cf99dd82
Varun Vachhar <varunvachhar@gmail.com>
2017-09-28 21:43:08 -0400

    ✨ hello world

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'public'),
   },
 };
